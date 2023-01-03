93dcfd469323dbebb45d4b2fec420752000ddf92
rayiqi <vindbaiq@gmail.com>
2023-01-03 18:57:21 +0700

    komponen

---
+++ src/belajar/apaja/Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component = ({name}) => {
+  return (
+    <div>
+    <h1> Bagaimana kabar {name}</h1>
+    </div>
+  )
+}
+
+export default Component
---
+++ src/belajar/apaja/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+// membuat komponen dengan classr
+
+class Header extends React.Component {
+    constructor() {
+      super();
+      // membuat objek state
+      this.state = {
+        title: "Belajar Reactjs",
+        subTitle: "Panduan step-by-step Reactjs untuk pemula"
+      };
+    }
+  
+    changeTitle = () => {
+      this.setState({
+        title: "Tutorial Reactjs Petani Kode"
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>{this.state.title}</h1>
+          <h2>{this.state.subTitle}</h2>
+          <button onClick={this.changeTitle}>Ubah Judul</button>
+        </div>
+      );
+    }
+  }
+  
+  // render komponen ke RealDOM
+//   ReactDOM.render(<Header />, document.getElementById("root"));
+  
+  export default Header
